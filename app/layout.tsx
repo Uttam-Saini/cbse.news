94d72e60897e727243b58d2644877576d3797cc5
Uttam Saini <uttam@Uttams-MacBook-Air.local>
2026-02-02 10:06:29 +0530

    Enable Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -34,6 +35,7 @@ export default function RootLayout({
         <main className="flex-1 transition-colors duration-300">{children}</main>
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
